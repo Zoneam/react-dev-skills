e098d4edcbb043aaa5dc20ba745926ac1cb224d6
Zoneam <zoneam@gmail.com>
2022-06-02 15:45:48 -0700

    handling change

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import './App.css';
 import SkillList from './SkillList';
 import NewSkillForm from './NewSkillForm';
 import './style.css'
-
+import { useState } from 'react'
 function App() {
 
-  const skills = [
-    { name: "HTML", level: 5 },
-    { name: "CSS", level: 3 },
-    { name: "JavaScript", level: 4 },
-    { name: "Python", level: 2 },
-  ];
+  const [skills, setSkills] = useState(
+    [
+      { name: "HTML", level: 5 },
+      { name: "CSS", level: 3 },
+      { name: "JavaScript", level: 4 },
+      { name: "Python", level: 2 },
+    ]
+  )
+
+  const addSkill = (skill) => {
+    setSkills([...skills, skill]);
+  }
 
   return (
     <div className="App teal-text padding-0">
       <h1>React Dev Skills</h1>
       <SkillList skills={skills}/>
       <hr/>
-      <NewSkillForm/>
+      <NewSkillForm addSkill={addSkill}/>
     </div>
   );
 }
--- src/SkillListItem.jsx
+++ src/SkillListItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './SkillListItem.css'
 
 export default function SkillListItem({skill}){
-    console.log(skill)
     return (
         <li className='SkillListItem'>{skill.name} <span className='level'>Level <strong>{skill.level}</strong></span></li>
     )
